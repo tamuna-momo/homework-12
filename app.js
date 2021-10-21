bf481ac4f811a1d3cf21460499fc66dd8fadb6d4
tamuna-momo <tamuna-momo@users.noreply.github.com>
2021-10-21 14:12:22 +0400

    add html, js. make formulas

---
+++ app.js
@@ -0,0 +1,77 @@
+
+// homework
+// 1.
+
+let myFriend="natia";
+let friendNumber=6;
+let petCat=true;
+let petDog=false;
+let withoutFriend=null;
+let notDefined= undefined;
+
+// 2.
+
+
+
+let add=200+123;
+let sub=450-200;
+let multiply=34*23;
+let div=125/23;
+let module=450 % 5;
+
+
+
+// 3.
+
+let str1='wow';
+let str2='js';
+let srt3='He came bathed in light';
+let str4='and the splendor and glory';
+
+
+
+console.log(str1+ str2);
+console.log(srt3+str4);
+
+console.log(`${str1} ${str2}, here we go`);
+console.log(`${srt3} ${str4}`);
+
+// 4.
+
+let lengthRectangle=7;
+let widhtRectangle=5;
+
+console.log(lengthRectangle * widhtRectangle);
+
+// 5.
+
+function calcHeight(a, b) {
+    return (Math.sqrt((b * b) - ((a/2) * (a/2))));
+  }
+  console.log(calcHeight(5,6));
+
+ function calcArea(a,h){
+      return (a*h/2);
+    
+  }
+  console.log(calcArea(5, 5.45));
+
+//   6.
+
+function calcCircumarea(r) {
+    return ((Math.PI)*(r*r));
+}
+console.log(calcCircumarea(7));
+
+
+
+
+
+
+
+
+
+
+
+
+
